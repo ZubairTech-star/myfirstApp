bb20959cb80de662879dbbb149481911d8a1880c
ZubairTech-star <104028955+ZubairTech-star@users.noreply.github.com>
2022-04-21 23:28:42 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "My First App With Feature Branch *******",
+            style: TextStyle(
+              fontSize: 25,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: (Colors.pink),
+        ),
+        body: const Center(child: Text("empty body")),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('clicked');
+          },
+          child: const Icon(Icons.add,),
+          backgroundColor: Colors.pink,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      ),
+    );
+  }
+}
